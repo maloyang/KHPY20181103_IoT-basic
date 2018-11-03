7fe6ab04762427cfdaf403af6c1e754bc38b286d
maloyang <33815331+maloyang@users.noreply.github.com>
2018-11-03 11:27:56 +0800

    Add files via upload

--- demo/02-2_DHT11_TODO2.py
+++ demo/02-2_DHT11_TODO2.py
@@ -3,16 +3,15 @@ from machine import Pin
 import time
 
 d = dht.DHT11(Pin(5)) #D1
+led = Pin(2, Pin.OUT) #D4
 
-while True:
+while(True):
     d.measure()
     T = d.temperature() # eg. 23 (℃)
     H = d.humidity()    # eg. 41 (% RH)
     print("T=%s, H=%s" %(T, H))
-
-    if T>30:
-        ...
+    if T>=25:
+        led.value(0) #ON
     else:
-        ...
+        led.value(1) #OFF
     time.sleep(1)
-    
